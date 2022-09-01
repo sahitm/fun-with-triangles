4a0ac282a0911114f515c9cd506189195c0fd0f6
sahitm <sahit7varma@gmail.com>
2022-09-01 23:43:56 +0530

    feat -added feature to calculate hypothenuse

---
+++ hypotenuse.js
@@ -0,0 +1,19 @@
+const input = document.querySelectorAll('.input')
+const hypotenuse = document.querySelector('#hypotenuse')
+const message = document.querySelector('.message')
+
+function handleHypo(a,b){
+    return Math.sqrt(a*a + b*b)
+}
+
+function calcHypotenuse(){
+    if(input[0].value && input[1].value >= 0){
+        const Hypo = handleHypo(input[0].value, input[1].value)
+        message.innerHTML = `The length of hypotenuse is ${Hypo}`
+    }else{
+        message.innerHTML = `Invalid Data`
+    }
+
+}
+
+hypotenuse.addEventListener('click',calcHypotenuse)
