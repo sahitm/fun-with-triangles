4e5917ba1bae04797ab407ecb6045e9a279cd056
sahitm <sahit7varma@gmail.com>
2022-09-02 00:04:29 +0530

    feat - added feat to calculate area

---
+++ areaOfTriangle.js
@@ -0,0 +1,25 @@
+const input = document.querySelectorAll('.input')
+const area = document.querySelector('#area')
+const message = document.querySelector('.message')
+
+
+
+function Area(a,b,c){
+    const s = (a+b+c)/2
+    console.log(s)
+    console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)))
+    return Math.sqrt(s*(s-a)*(s-b)*(s-c))
+}
+
+function calcArea(){
+    console.log(input[0].value,input[1].value,input[2].value)
+    if(input[0].value && input[1].value && input[2].value >= 0){
+        const area = Area(Number(input[0].value) , Number(input[1].value)  , Number(input[2].value) )
+        message.innerHTML = `The area of triangle is ${area}`
+    }else{
+        message.innerHTML = `Invalid Data`
+    }
+
+}
+
+area.addEventListener('click',calcArea)
